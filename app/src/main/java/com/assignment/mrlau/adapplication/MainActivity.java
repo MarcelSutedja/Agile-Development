5ab0af6c31cd4b7bc9aed907acdda07af002480e
asiansnorlax <alex.tzh@outlook.com>
2019-05-07 23:23:36 +0800

    Updated the GPA Calculator Made the logout button below

--- app/src/main/java/com/assignment/mrlau/adapplication/MainActivity.java
+++ app/src/main/java/com/assignment/mrlau/adapplication/MainActivity.java
@@ -8,9 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,6 +17,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -109,6 +110,26 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
         FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_first_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new FirstFragment())
+                    .commit();
+
+        } else if (id == R.id.nav_second_layout) {
+            Intent goToGpaCalc = new Intent(getApplicationContext(),Gpa.class);
+            startActivity(goToGpaCalc);
+        } else if (id == R.id.nav_third_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new ThirdFragment())
+                    .commit();
+        }else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
         if (id == R.id.nav_logout){
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setMessage("Confirm Logout").setCancelable(true)
@@ -127,25 +148,6 @@ public class MainActivity extends AppCompatActivity
                                     dialog.cancel();
                                 }
                             }).show();
-        }
-        else if (id == R.id.nav_first_layout) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame
-                            , new FirstFragment())
-                    .commit();
-
-        } else if (id == R.id.nav_second_layout) {
-            Intent goToGpaCalc = new Intent(getApplicationContext(),Gpa.class);
-            startActivity(goToGpaCalc);
-        } else if (id == R.id.nav_third_layout) {
-            fragmentManager.beginTransaction()
-                    .replace(R.id.content_frame
-                            , new ThirdFragment())
-                    .commit();
-        }else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
